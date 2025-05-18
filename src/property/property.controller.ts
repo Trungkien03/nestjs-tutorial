5c4ad788dfdc8b62da9e8a0a861c21b585d6709f
Kien Nguyen <trungkiennguyen0310@gmail.com>
2025-05-18 17:30:22 +0700

    feat: add new example property module

---
+++ src/property/property.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('property')
+export class PropertyController {
+  @Get()
+  findAll() {
+    return 'All properties';
+  }
+
+  @Get(':id/:slug')
+  findOne(@Param('id') id: string, @Param('slug') slug: string) {
+    return { id, slug };
+  }
+
+  @Post()
+  create(@Body() body: { name: string; description: string }) {
+    return body;
+  }
+}
